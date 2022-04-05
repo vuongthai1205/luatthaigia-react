6624f4569288c045723a08d2c2540fd91903deaf
vuongthai1205 <84846136+vuongthai1205@users.noreply.github.com>
2022-04-05 11:55:28 +0700

    Vuong commit lan 3 5/4/2022

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,132 @@
+import { useState } from "react";
 
 function Form(){
+    const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
+  const [tittle, setTittle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let errorForm = {}
+    if(name.trim() === '')
+    {
+        errorForm['name'] = 'Họ và tên không được để trống'
+    }
+    if(email.trim() === '')
+    {
+        errorForm['email'] = 'Email không được để trống'
+    }
+    if(tel.trim() === '')
+    {
+        errorForm['tel'] = 'Số điện thoại không được để trống'
+    }
+    if(tittle.trim() === '')
+    {
+        errorForm['tittle'] = 'Tiêu đề không được để trống'
+    }
+    if(description.trim() === '')
+    {
+        errorForm['description'] = 'Mô tả không được để trống'
+    }
+    console.log(errorForm.name)
+    if (Object.keys(errorForm) === 0)
+    {
+        let data = {
+            'entry.341284223': name,
+            'entry.1894415154' : email,
+            'entry.239575575': tel,
+            'entry.1507243237':tittle,
+            'entry.662749957': description
+        }
+        let queryString = new URLSearchParams(data);
+        queryString = queryString.toString();
+        
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfIAKtt9g0aQVDPkM2k3u4BEUTpF9CVH6xIDUwEnTDRGY-jQg/formResponse', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(queryString);
+        setName('')
+        setEmail('')
+        setTel('')
+        setTittle('')
+        setDescription('')
+    }
+    else{
+        console.log('nhập đầy đủ');
+        setName('')
+        setEmail('')
+        setTel('')
+        setTittle('')
+        setDescription('')
+    }}
     
     return (
         <section className="form-container" id="form1">
             <div className="form grid wide">
                 <div className="row">
-                    <form action="" method="post" className="col l-8 c-12 m-12">
-                        <div className="form__item">
-                            <input type="text" name="name" required id="form__name" placeholder="Họ và tên..."/>
-                            <span className="form__required"></span>
-                        </div>
-                        <div className="form__item">
-                            <input type="email" name="email" required placeholder="Email của bạn..."/>
-                            <span className="form__required"></span>
-                        </div>
-                        <div className="form__item">
-                            <input type="tel" name="number" required placeholder="Số điện thoại..."/>
-                            <span className="form__required"></span>
-                        </div>
-                        <div className="form__item">
-                            <input type="tel" name="tittle" required placeholder="Tiêu đề..."/>
-                            <span className="form__required"></span>
-                        </div>
-                        <div className="form__item">
-                            <input type="text" name="description" required placeholder="Mô tả vấn đề của bạn..."/>
-                            <span className="form__required"></span>
-                        </div>
-                        <div className="form__notify"></div>
-                        <button className="btn" id="form__submit">
-                                <i className="fa-solid fa-paper-plane"></i>
-                        </button>
-                    </form>
+                <form action="" method="post" className="col l-8 c-12 m-12">
+                    <div className="form__item">
+                        <input
+                            type="text"
+                            name="name"
+                            required id="form__name"
+                            placeholder="Họ và tên..."
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <span className="form__required"></span>
+                    </div>
+                    <div className="form__item">
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            placeholder="Email của bạn..."
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <span className="form__required"></span>
+                    </div>
+                    <div className="form__item">
+                        <input 
+                            type="tel"
+                            name="number"
+                            required
+                            placeholder="Số điện thoại..."
+                            value={tel}
+                            onChange={(e) => setTel(e.target.value)}
+                        />
+                        <span className="form__required"></span>
+                    </div>
+                    <div className="form__item">
+                        <input
+                            type="tel"
+                            name="tittle"
+                            required
+                            placeholder="Tiêu đề..."
+                            value={tittle}
+                            onChange={(e) => setTittle(e.target.value)}
+                        />
+                        <span className="form__required"></span>
+                    </div>
+                    <div className="form__item">
+                        <input
+                            type="text"
+                            name="description"
+                            required
+                            placeholder="Mô tả vấn đề của bạn..."
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <span className="form__required"></span>
+                    </div>
+                    <div className="form__notify"></div>
+                    <button className="btn" id="form__submit" onClick={handleSubmit}>
+                        <i className="fa-solid fa-paper-plane"></i>
+                    </button>
+                </form>
                     <div className="col l-4 c-12 m-12">
                         <div className="info-form">
                             <h2>Thông tin liên hệ</h2>
